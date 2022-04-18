83587fab28abbb3dfab33ecdcb6bb55db1bdb2ec
Jessee Chow <admin@cnbbx.com>
2022-04-18 20:24:16 +0800

    Create first.sh

---
+++ first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#=============================================================
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: cnbbx
+#=============================================================
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default 
